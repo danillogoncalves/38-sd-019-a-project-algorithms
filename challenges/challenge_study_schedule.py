b3b73b0847eb47daa28c466895afc68409bd3cc2
danillogoncalves <danillogoncalves001@gmail.com>
2022-11-19 20:42:45 -0300

    feat: :white_check_mark: Requisito 1 feito.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int) or target_time == 0:
+        return None
+    count = 0
+
+    for entryTime, departureTime in permanence_period:
+        if not isinstance(entryTime, int) or not isinstance(
+            departureTime, int
+        ):
+            return None
+        if entryTime <= target_time <= departureTime:
+            count += 1
+
+    return count
