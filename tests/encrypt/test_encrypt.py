ed142ab576673ffdbc40b3de124730b4d45e52ff
danillogoncalves <danillogoncalves001@gmail.com>
2022-11-19 21:08:04 -0300

    feat: :white_check_mark: Requisito 2 feito.

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("AABBCC", 3) == "BAA_CCB"
+
+    assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("AABBCC", "-1")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(112345, 3)
